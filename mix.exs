dc6d0973a4a78abc1e56c227d348e6d284a46f5f
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2016-01-23 21:28:13 +0900

    ex_doc

--- mix.exs
+++ mix.exs
@@ -36,7 +36,10 @@ defmodule Exromaji.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:ex_doc, "~> 0.11", only: :dev},]
+    [
+      {:earmark, "~> 0.1", only: :dev},
+      {:ex_doc, "~> 0.11", only: :dev},
+    ]
   end
 
   defp package do

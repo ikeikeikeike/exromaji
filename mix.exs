0dbd9b9447bce831f4243a0f561223a523a857fa
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-10-04 15:01:27 +0900

    Bumps version

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Exromaji.Mixfile do
   def project do
     [ app: :exromaji,
       name: "Exromaji",
-      version: "0.3.0",
+      version: "0.4.0",
       elixir: ">= 1.0.0",
       source_url: "https://github.com/ikeikeikeike/exromaji",
       docs: [extras: ["README.md"]],
